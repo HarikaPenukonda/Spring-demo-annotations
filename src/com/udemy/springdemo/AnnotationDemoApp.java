4f31ff8c3bca437452bd06d6e09638debbe42b7e
harika penukonda <penukondaharika@gmail.com>
2020-07-15 11:34:01 -0500

    Create AnnotationDemoApp.java

---
+++ src/com/udemy/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,20 @@
+package com.udemy.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// get the bean from spring container
+		Coach theCoach  = context.getBean("thatsillycoach", Coach.class);
+		// call a method of a bean
+		System.out.println(theCoach.getDailyWorkout());
+		// close context
+		context.close();
+
+	}
+
+}
