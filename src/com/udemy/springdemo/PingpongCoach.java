415d9ac6898585ec8d4854f2aae9ca773cf19d1c
harika penukonda <penukondaharika@gmail.com>
2020-07-15 11:56:21 -0500

    Create PingpongCoach.java

---
+++ src/com/udemy/springdemo/PingpongCoach.java
@@ -0,0 +1,18 @@
+package com.udemy.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PingpongCoach implements Coach {
+	
+	public PingpongCoach()
+	{
+		System.out.println(" PingPongCoach: inside default constructor ");
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your pingpong drop shot";
+	}
+
+}
