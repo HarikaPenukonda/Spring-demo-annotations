b52197fa41f06c306258a986f6f24d23369cefb1
harika penukonda <penukondaharika@gmail.com>
2020-07-16 15:08:31 -0500

    add methods related to swim class

--- src/com/udemy/springdemo/SwimJavaConfigDemoApp.java
+++ src/com/udemy/springdemo/SwimJavaConfigDemoApp.java
@@ -9,11 +9,13 @@ public class SwimJavaConfigDemoApp {
 		// read the spring config java class
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		// get the bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		// call a method of a bean
 		System.out.println(theCoach.getDailyWorkout());
 		// call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getEmail());
+		System.out.println(theCoach.getTeam());
 		// close context
 		context.close();
 
